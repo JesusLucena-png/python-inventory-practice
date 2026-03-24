fcdbf820cf94d955ca0561fcf85a0f87f3995e00
JesusLucena-png <lucenaquinterojesusdavid@gmail.com>
2026-03-23 20:55:47 -0500

    app productos

--- gestion_productos.py
+++ gestion_productos.py
@@ -24,11 +24,36 @@ def display_inventory_list(inventory_list):
     for enumeration , record  in enumerate(inventory_list):
         print(f"{enumeration+1}. {record}")
 
+def error_duplicate_data(user_input, available_categories, list_product_category):
+    print(available_categories[user_input-1])
+    print(list_product_category)
 
+    if available_categories[user_input-1] in list_product_category:
+        return("\n\033[1;31m" + "-"*60 + f"\nError: This data is already duplicated." + "\n" + "-"*60 + "\033[0m")
+    
+    return None
 
+def confirm_exit(message="Press (n) to finish and any key to continue: "):
+    """
+    Function: confirm_exit
 
+    Parameters:
 
+    - message:
+    Text message displayed to the user to decide whether to continue
+    or exit a loop. By default, it prompts the user to press (n) to finish
+    or any other key to continue.
 
+    Description:
+    This function displays a confirmation message that allows the user
+    to either continue or terminate a loop (e.g., a while loop) based
+    on their input.
+    """
+
+    user_option = input(f"\n\033[31m >> \033[0m{message}").strip().lower()
+        
+    if user_option == "n":
+        return True     
 
 
 
@@ -36,97 +61,120 @@ def display_inventory_list(inventory_list):
 
 
 def add_product(inventory, available_categories, title):
+
     product_history = {}
 
-    while True:
+    act_add_product = True
+    while act_add_product:
+        title(f"Add Product N°{len(product_history)+1} - ID{len(inventory)+1}")
 
-        product_name = input("\n\033[34m >> \033[0mPlease enter the product name: ")
+        while True:
 
-        error = error_empty_field(product_name)
-        if error:
-            print(error)
-            continue 
+            product_name = input("\n\033[34m >> \033[0mPlease enter the product name: ")
 
-        break
+            error = error_empty_field(product_name)
+            if error:
+                print(error)
+                continue 
 
-    while True:
+            break
 
-        try:
-            product_price = float(input("\n\033[34m >> \033[0mPlease enter the product price: "))
+        while True:
+
+            try:
+                product_price = float(input("\n\033[34m >> \033[0mPlease enter the product price: "))
+                
+            except ValueError:
+                print("\n\033[1;31m" + "-"*60 + f"\nError: Please enter numbers only in this field." + "\n" + "-"*60 + "\033[0m")
+                continue
+
+            error = error_empty_field(product_price)
+            if error:
+                print(error)
+                continue 
+
+            error = error_negative_value(product_price)
+            if error:
+                print(error)
+                continue 
+
+            break
             
-        except ValueError:
-            print("\n\033[1;31m" + "-"*60 + f"\nError: Please enter numbers only in this field." + "\n" + "-"*60 + "\033[0m")
-            continue
+        while True:
 
-        error = error_empty_field(product_price)
-        if error:
-            print(error)
-            continue 
+            try:
+                product_stock = int(input("\n\033[34m >> \033[0mPlease enter the product stock quantity: "))
+                
+            except ValueError:
+                print("\n\033[1;31m" + "-"*60 + f"\nError: Please enter numbers only in this field." + "\n" + "-"*60 + "\033[0m")
+                continue
 
-        error = error_negative_value(product_price)
-        if error:
-            print(error)
-            continue 
+            error = error_empty_field(product_stock)
+            if error:
+                print(error)
+                continue
 
-        break
-        
-    while True:
+            error = error_negative_value(product_stock)
+            if error:
+                print(error)
+                continue 
 
-        try:
-            product_stock = int(input("\n\033[34m >> \033[0mPlease enter the product stock quantity: "))
+            break   
+
+
+        list_product_category = []
+
+        title("List Category")
+
+        display_inventory_list(available_categories)
+        print("\033[34m-"*60)
+
+        print("\n\033[31mEnter '0' to finish.\033[0m")
             
-        except ValueError:
-            print("\n\033[1;31m" + "-"*60 + f"\nError: Please enter numbers only in this field." + "\n" + "-"*60 + "\033[0m")
+        while True:
+
+            try:
+                product_category = int(input(f"\n\033[34m >> \033[0mPlease select a category option for the product N°{len(list_product_category)+1}: "))
+            except ValueError:
+                print("\n\033[1;31m" + "-"*60 + f"\nError: Please enter numbers only in this field." + "\n" + "-"*60 + "\033[0m")
+                continue
+
+            error = error_empty_field(product_category)
+            if error:
+                print(error)
+                continue 
+
+            if product_category == 0:
+                if list_product_category:
+                    print(f"\n{len(list_product_category)+1} categories added successfully.")
+                    print(", ".join(list_product_category))
+                    break
+
+                print("\n\033[1;31m" + "-"*60 + f"\nError: You must select a category to classify the product." + "\n" + "-"*60 + "\033[0m")
+                continue
+
+            error = error_category_out_of_range(product_category, available_categories)
+            if error:
+                print(error)
+                continue 
+
+            error = error_duplicate_data(product_category, available_categories, list_product_category)
+            if error:
+                print(error)
+                continue 
+
+            list_product_category.append(available_categories[product_category-1]) 
+
+        add_product_inventory(product_name,product_price,product_stock,list_product_category, inventory, product_history)
+
+        # Ask user if they want to continue
+        exit = confirm_exit()
+        if exit == True:
+            act_add_product = False
+        else:
             continue
 
-        error = error_empty_field(product_stock)
-        if error:
-            print(error)
-            continue
-
-        error = error_negative_value(product_stock)
-        if error:
-            print(error)
-            continue 
-
-        break   
-
-
-    list_product_category = []
-    title("List Category")
-
-    display_inventory_list(available_categories)
-    print("\033[34m-"*60)
-    print("\n\033[31mEnter '0' to finish.\033[0m")
-        
-    while True:
-
-        try:
-            product_category = int(input(f"\n\033[34m >> \033[0mPlease select a category option for the product N°{len(list_product_category)+1}: "))
-        except ValueError:
-            print("\n\033[1;31m" + "-"*60 + f"\nError: Please enter numbers only in this field." + "\n" + "-"*60 + "\033[0m")
-            continue
-
-        error = error_empty_field(product_category)
-        if error:
-            print(error)
-            continue 
-
-        if product_category == 0:
-            if not list_product_category == False:
-                print("q")
-                break
-
-        error = error_category_out_of_range(product_category, available_categories)
-        if error:
-            print(error)
-            continue 
-
-        list_product_category.append(available_categories[product_category-1]) 
-
-    add_product_inventory(product_name,product_price,product_stock,list_product_category, inventory, product_history)
-
-    print(product_history)
+        print(inventory)
 
 def add_product_inventory(product_name, product_price, product_stock, product_category, inventory, history):
 
@@ -148,7 +196,6 @@ def add_product_inventory(product_name, product_price, product_stock, product_ca
         "status" : True
     }
 
-    print(inventory)
 
 def edit_product(inventory):
     # EN: Edits an existing product in the inventory
